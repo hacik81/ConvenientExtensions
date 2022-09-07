7f7537cfe33681561b62edba33145034a1624278
Sebastian Hat <43092885+hacik81@users.noreply.github.com>
2022-09-07 13:45:53 +0200

    Initial Commit

---
+++ Tests/ConvenientExtensionsTests/ConvenientExtensionsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ConvenientExtensions
+
+final class ConvenientExtensionsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ConvenientExtensions().text, "Hello, World!")
+    }
+}
